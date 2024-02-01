1d7940e51f421395f2e4c9e3e22445c4bb1d756a
twager <trey.d.wager.civ@army.mil>
2024-02-01 10:19:33 -0700

    add some bois

---
+++ nvim/lua/plugins/custom_lazy.lua
@@ -0,0 +1,115 @@
+return {
+  -- disable indent
+  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
+  { "jay-babu/mason-nvim-dap.nvim", enabled = false },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "pyright",
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    -- dependencies = {
+    --   "nvim-lua/plenary.nvim",
+    --   "antoinemadec/FixCursorHold.nvim",
+    --   "nvim-treesitter/nvim-treesitter",
+    -- },
+    opts = {
+      adapters = {
+        ["neotest-python"] = {
+          dap = { just_my_code = false },
+          -- args = { "-n", "10" },
+          pytest_discover_instances = true,
+        },
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    config = function()
+      require("dap-python").setup("/opt/venv/bin/python")
+      require("dap.ext.vscode").load_launchjs()
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        python = { "ruff_format" },
+      },
+      formatters = {
+        ruff_format = {
+          prepend_args = { "format", "--line-length=120" },
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+  -- Use <tab> for completion and snippets (supertab)
+  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+  -- then: setup supertab in cmp
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-emoji",
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      local luasnip = require("luasnip")
+      local cmp = require("cmp")
+
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- this way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+    end,
+  },
+}
