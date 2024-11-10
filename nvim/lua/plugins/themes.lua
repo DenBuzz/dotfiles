0fcc138b9f9f68be4a8ba1c3bd5f11a79bb77c1b
twager <trey.d.wager.civ@army.mil>
2024-11-10 09:30:13 -0700

    themes

--- nvim/lua/plugins/themes.lua
+++ nvim/lua/plugins/themes.lua
@@ -1,114 +1,114 @@
 return {
-  { "tanvirtin/monokai.nvim", lazy = false, priority = 1000 },
-  {
-    "oxfist/night-owl.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    options = { transparent = true },
-  },
-  {
-    "Shatur/neovim-ayu",
-    lazy = false,
-    priority = 1000,
-    -- for transparent_background
-    config = function()
-      require("ayu").setup({
-        overrides = {
-          normal = { bg = "None" },
-          ColorColumn = { bg = "None" },
-          SignColumn = { bg = "None" },
-          Folded = { bg = "None" },
-          FoldColumn = { bg = "None" },
-          CursorLine = { bg = "None" },
-          CursorColumn = { bg = "None" },
-          WhichKeyFloat = { bg = "None" },
-          VertSplit = { bg = "None" },
-        },
-      })
-    end,
-  },
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    priority = 1000,
-    name = "catppuccin",
-    opts = {
-      transparent_background = true,
-      integrations = {
-        aerial = true,
-        alpha = true,
-        cmp = true,
-        dashboard = true,
-        flash = true,
-        gitsigns = true,
-        headlines = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        leap = true,
-        lsp_trouble = true,
-        mason = true,
-        markdown = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        neotree = true,
-        noice = true,
-        -- notify = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        treesitter_context = true,
-        which_key = true,
-      },
+    { "tanvirtin/monokai.nvim", lazy = false, priority = 1000 },
+    {
+        "oxfist/night-owl.nvim",
+        lazy = true, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        options = { transparent = true },
     },
-  },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = { style = "night", transparent = true },
-  },
-  {
-    "ellisonleao/gruvbox.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require("gruvbox").setup({
-        transparent_mode = true,
-      })
-    end,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-  },
-  {
-    "EdenEast/nightfox.nvim",
-    opts = {
-      options = {
-        transparent = true,
-        modules = {
-          aerial = true,
-          which_key = true,
-          treesitter = true,
-          neotree = true,
-          neotest = true,
+    {
+        "Shatur/neovim-ayu",
+        lazy = true,
+        priority = 1000,
+        -- for transparent_background
+        -- config = function()
+        --     require("ayu").setup({
+        --         overrides = {
+        --             normal = { bg = "None" },
+        --             ColorColumn = { bg = "None" },
+        --             SignColumn = { bg = "None" },
+        --             Folded = { bg = "None" },
+        --             FoldColumn = { bg = "None" },
+        --             CursorLine = { bg = "None" },
+        --             CursorColumn = { bg = "None" },
+        --             WhichKeyFloat = { bg = "None" },
+        --             VertSplit = { bg = "None" },
+        --         },
+        --     })
+        -- end,
+    },
+    {
+        "catppuccin/nvim",
+        lazy = true,
+        priority = 1000,
+        name = "catppuccin",
+        opts = {
+            transparent_background = true,
+            integrations = {
+                aerial = true,
+                alpha = true,
+                cmp = true,
+                dashboard = true,
+                flash = true,
+                gitsigns = true,
+                headlines = true,
+                illuminate = true,
+                indent_blankline = { enabled = true },
+                leap = true,
+                lsp_trouble = true,
+                mason = true,
+                markdown = true,
+                mini = true,
+                native_lsp = {
+                    enabled = true,
+                    underlines = {
+                        errors = { "undercurl" },
+                        hints = { "undercurl" },
+                        warnings = { "undercurl" },
+                        information = { "undercurl" },
+                    },
+                },
+                navic = { enabled = true, custom_bg = "lualine" },
+                neotest = true,
+                neotree = true,
+                noice = true,
+                -- notify = true,
+                semantic_tokens = true,
+                telescope = true,
+                treesitter = true,
+                treesitter_context = true,
+                which_key = true,
+            },
         },
-      },
     },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "catppuccin",
+    {
+        "folke/tokyonight.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = { style = "night", transparent = false },
     },
-  },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = true,
+        priority = 1000,
+        -- config = function()
+        --     require("gruvbox").setup({
+        --         transparent_mode = true,
+        --     })
+        -- end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+    },
+    {
+        "EdenEast/nightfox.nvim",
+        opts = {
+            options = {
+                transparent = false,
+                modules = {
+                    aerial = true,
+                    which_key = true,
+                    treesitter = true,
+                    neotree = true,
+                    neotest = true,
+                },
+            },
+        },
+    },
+    {
+        "LazyVim/LazyVim",
+        opts = {
+            colorscheme = "tokyonight",
+        },
+    },
 }
