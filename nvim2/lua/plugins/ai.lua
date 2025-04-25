06404d24350006c5f10f94957190b95fa553e9a4
DenBuzz <trey@wagerfamily.com>
2025-04-25 07:06:25 -0600

    formatting

---
+++ nvim2/lua/plugins/ai.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "olimorris/codecompanion.nvim",
+    opts = {
+      strategies = {
+        chat = {
+          adapter = "ollama",
+        },
+        inline = {
+          adapter = "ollama",
+        },
+        cmd = {
+          adapter = "ollama",
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+  },
+}
--- nvim2/lua/plugins/formatting.lua
+++ nvim2/lua/plugins/formatting.lua
@@ -3,7 +3,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ["python"] = { "ruff" },
+        ["python"] = { "ruff", "ruff_format" },
       },
     },
   },
